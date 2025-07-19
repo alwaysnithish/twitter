703f15c519de31b222b48c2f103da7bb2919d848
alwaysnithish <inithish21k@gmail.com>
2025-07-19 15:28:11 +0530

    Update urls.py

--- tesla/urls.py
+++ tesla/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
     path('', views.TwitterVideoDownloader.as_view(), name='index'),
     
     # CSRF token endpoint
-    path('get-csrf-token/', views.get_csrf_token, name='get_csrf_token'),
+    path('get_csrf_token/', views.get_csrf_token, name='get_csrf_token'),
     
     # Download endpoint - handles POST requests to stream video downloads
-    path('download-video-stream/', views.download_video_stream, name='download_video_stream'),
+    path('download_video_stream/', views.download_video_stream, name='download_video_stream'),
     
     # Cookies status endpoint - checks if cookies file exists
-    path('cookies-status/', views.cookies_status, name='cookies_status'),
+    path('cookies_status/', views.cookies_status, name='cookies_status'),
     
     # Video info endpoint - gets additional video metadata
-    path('video-info/', views.video_info, name='video_info'),
+    path('video_info/', views.video_info, name='video_info'),
 ]
 
 if settings.DEBUG:
